de208e5321969866872f8f931aa251d705dd4058
Raúl Garay Silva <ragasi1992@gmail.com>
2024-05-24 20:40:28 -0600

    update dockerfile and docker-compose

--- web/vite.config.js
+++ web/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     },
     proxy: {
       '^/api': {
-        target: 'http://api',
+        target: 'http://backend:5000',
         changeOrigin: true
       }
     }
