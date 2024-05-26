22f128b170e84e1cec51f97d6d0a8acb7debf91f
Raúl Garay Silva <ragasi1992@gmail.com>
2024-05-25 21:28:52 -0600

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     },
     proxy: {
       '^/api': {
-        target: 'http://backend',
+        target: 'http://backend:5000',
         changeOrigin: true
       }
     }
